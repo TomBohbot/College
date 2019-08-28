7c9908bd25b198ca70d08938f6be9ac78f29df81
Tom <tbohbot@mail.yu.edu>
2019-08-28 17:03:30 -0400

    Assignment 1 - HelloWorld

---
+++ IntroToCompSci/assignments/HelloWorld/src/main/java/edu/yu/intro/HelloWorld.java
@@ -0,0 +1,14 @@
+package edu . yu . intro ;
+
+/** " Hello World " for Yeshiva University .
+*
+* @author Avraham Leff
+*/
+public class HelloWorld {
+	public static void main ( final String [] args ) {
+		System . out . println (" Hello World !") ;
+		System . out . println (" The world stands on 3 pillars ");
+		System . out . println (" Not 2.52 pillars ");
+		System . out . println (" and ... this is a very true statement ");
+	}
+}
