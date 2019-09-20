b64b01b94b084f07327f6e6cf877f56c86b37e88
Tom <tbohbot@mail.yu.edu>
2019-09-20 12:14:18 +0300

    VerifyRandom with accurate 1/36

---
+++ IntroToCompSci/assignments/VerifyRandom/src/main/java/edu/yu/intro/TestVerify.java
@@ -0,0 +1,19 @@
+/** " Verify Random " Assignment
+*
+* @author Tom Bohbot
+*/
+
+import java.util.Random;
+
+public class TestVerify {
+	public static void main (final String [] args) {
+
+		double predictNumb = 1/36;
+		double actualNumb  = 0.030;
+		double dividingBoth = actualNumb - predictNumb
+		double leffEquation = (dividingBoth / predictNumb)
+
+		System.out.println ("This is your predicted number in decimals: " + predictNumb);
+		System.out.println ("This is your actual number in decimals:    " + actualNumb);
+		System.out.println ("This is your them subtractd in decimals:   " + dividingBoth);
+		System.out.println ("This is Leff's equation:                   " + leffEquation);
