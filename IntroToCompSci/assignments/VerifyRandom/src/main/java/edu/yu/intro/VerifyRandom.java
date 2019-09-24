9a13a9fab16df88b5d2058bfa076208c607e1c1b
Tom <tbohbot@mail.yu.edu>
2019-09-24 15:47:16 -0400

    Modified so that it is accurate looping

--- IntroToCompSci/assignments/VerifyRandom/src/main/java/edu/yu/intro/VerifyRandom.java
+++ IntroToCompSci/assignments/VerifyRandom/src/main/java/edu/yu/intro/VerifyRandom.java
@@ -12,22 +12,36 @@ public class VerifyRandom {
 		
 		double snakeEyes = 0;
 
-		for (int i = 1; i <= 10000; i++) {
+		int i = 0;
+		Random random = new Random ();
+		int diceOne = 0;
+		int diceTwo = 0;
+		double rollsUntilSnakeEyes = 0;
+		double totalCombinedRolls = 0;
+
+
+		for (i =1; i <=10000; i++){
+
+			while (snakeEyes < 10000) {
 		
-			Random random = new Random ();
-			int diceOne = random.nextInt (6) + 1;
-			int diceTwo = random.nextInt (6) + 1;
+				diceOne = random.nextInt (6) + 1;
+				diceTwo = random.nextInt (6) + 1;	
 
-			if (diceOne == 1 && diceTwo == 1)
-				snakeEyes = snakeEyes + 1;
+				rollsUntilSnakeEyes = rollsUntilSnakeEyes + 1;
 
-		}
+				if (diceOne == 1 && diceTwo == 1)
+					snakeEyes = (snakeEyes + 1);
+				
+			}	
+
+	}
 
 		int totalLoops = 10000;
-		double averageSimProb = snakeEyes / totalLoops;
-		double projectedRatio = 0.02777777777777777777777778;
+		double averageSimProb = 10000 / rollsUntilSnakeEyes;
+		double numOfFrac = 1;  									// Due to setting variable equal to "1/36" being zero.
+		double denOfFrac = 36;								    // Due to setting variable equal to "1/36" being zero.
+		double projectedRatio = numOfFrac / denOfFrac;
 		double difAsARatio = Math.abs((averageSimProb - projectedRatio) / projectedRatio);
-		double infitinityTest = (0.1 - (1/3)) / (1/3);
 
 		System.out.printf("%-65s %5.3f %n" , "Average simulated probability: " , averageSimProb);
 		System.out.printf("%-65s %5d %n" , "Number of simulations: " , totalLoops);
