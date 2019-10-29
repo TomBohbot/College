322bb78b905c269d8aee452fe2ee0a16329ed311
Tom <tbohbot@mail.yu.edu>
2019-10-28 20:10:21 -0400

    Assignment 8 v1

---
+++ IntroToCompSci/assignments/JUnit4Factorial/src/main/java/edu/yu/intro/Factorial.java
@@ -0,0 +1,80 @@
+package edu.yu.intro;
+
+/** " JUNIT4 " Assignment #8
+*
+* @author Tom Bohbot
+*/
+
+import java.util.Arrays;
+
+public class Factorial {
+
+	public static void main (final String [] args) {
+		long factorialTableMethod[] = factorialTable();
+	}
+
+	public static long factorial (int n) { 
+
+		int counter = 0;
+		int passedCounter = 0;
+		long totalFactorialValue = 0;
+		long factorValue[] = new long [21];
+
+		if (n < 0 || n > 20) {
+			throw new IllegalArgumentException ("Factorial Value must be an interger inbetween 0 and 20.");
+		}
+		while (counter <= n) {
+			if (counter == 0) {
+				factorValue[0] = 1;
+				totalFactorialValue = 1;
+				counter = 1;
+			}
+			else if (counter >= 1 && counter <= 9) {
+				totalFactorialValue = counter * factorValue[passedCounter];
+				factorValue [counter] = totalFactorialValue;
+				counter = counter + 1;
+				passedCounter = counter - 1;
+			}
+			else if (counter >= 10) {
+				totalFactorialValue = counter * factorValue[passedCounter];
+				factorValue [counter] = totalFactorialValue;
+				counter = counter + 1;
+				passedCounter = counter - 1;
+			}
+		}
+		return totalFactorialValue;
+	}
+
+	public static long[] factorialTable() {
+		System.out.println("********************");
+		int counter = 0;
+		int passedCounter = 0;
+		long totalFactorialValue = 0;
+		long factorValue [] = new long [21];
+
+		while (counter <= 20) {
+			if (counter == 0) {
+				factorValue[0] = 1;
+				totalFactorialValue = 0;
+				System.out.println("Factorial ( " + counter + ") = " + factorValue[counter]);
+				counter = 1;
+			}
+			if (counter >= 1 && counter <= 9) {
+				totalFactorialValue = counter * factorValue[passedCounter];
+				factorValue [counter] = totalFactorialValue;
+				System.out.println("Factorial ( " + counter + ") = " + factorValue[counter]);
+				counter = counter + 1;
+				passedCounter = counter - 1;
+			}
+			if (counter >= 10) {
+				totalFactorialValue = counter * factorValue[passedCounter];
+				factorValue [counter] = totalFactorialValue;
+				System.out.println("Factorial (" + counter + ") = " + factorValue[counter]);
+				counter = counter + 1;
+				passedCounter = counter - 1;
+			}
+		}
+		System.out.println("********************");
+		return factorValue;
+	}
+}
---
+++ IntroToCompSci/assignments/JUnit4Factorial/src/test/java/edu/yu/intro/test/FactorialTest.java
@@ -0,0 +1,47 @@
+package edu.yu.intro.test;
+
+/** " ExceptionalEmployeePay " Assignment #6
+*
+* @author Tom Bohbot
+*/
+
+import edu.yu.intro.Factorial;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class FactorialTest {
+
+	@Test
+	public void factorial0 () {
+		assertEquals ("testing factorial 0" , 1L , Factorial.factorial(0));
+	}
+	@Test
+	public void factorial1 () {
+		assertEquals ("testing factorial 1" , 1L , Factorial.factorial(1));
+	}
+	@Test
+	public void factorial3 () {
+		assertEquals ("testing factorial 9" , 362880L , Factorial.factorial(9));
+	}
+	@Test
+	public void factorial10 () {
+		assertEquals ("testing factorial 10" , 3628800L , Factorial.factorial(10));
+	}
+	@Test
+	public void factorial19 () {
+		assertEquals ("testing factorial 19" , 121645100408832000L , Factorial.factorial(19));
+	}
+	@Test
+	public void factorial20 () {
+		assertEquals ("testing factorial 20" , 2432902008176640000L , Factorial.factorial(20));
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void invalidInputTooSmall () {
+		Factorial.factorial(-1);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void invalidInputTooLarge () {
+		Factorial.factorial(21);
+	}
+
+}
