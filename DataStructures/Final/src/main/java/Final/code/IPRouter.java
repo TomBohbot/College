b98205bd31e95ad529f31fa16fa537dba89bc344
BohbotTom <tbohbot@mail.yu.edu>
2020-05-26 17:07:11 -0700

    Working Version of Final

---
+++ DataStructures/Final/src/main/java/Final/code/IPRouter.java
@@ -0,0 +1,156 @@
+package Final.code;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * IPRouter simulates the decision process for an IP router dispatching packets
+ * according a prefix trie of routing rules.
+ * 
+ * @author Van Kelly
+ * @version 1.0
+ */
+public class IPRouter {
+    final int nPorts; 
+    final int cacheSize;
+    final BitVectorTrie<Integer> trie = new BitVectorTrie<>();
+    private HashMap <Integer , IPAddress> map = new HashMap <Integer , IPAddress> ();
+    private HashMap <IPAddress , Integer> bigMap = new HashMap <IPAddress , Integer> ();
+    private RouteCache routeCache;
+    private int cacheCounter;
+
+    /** Router constructor
+     * @param nPorts    the number of output ports, numbered 0 ... nPorts-1.  Pseudo-port -1 is 
+     *                  always used for errors.
+     * @param cacheSize the number of IP Addresses to be kept in a cache of the most recently routed 
+     *                  UNIQUE IP Addresses
+     */
+    public IPRouter (int nPorts, int cacheSize) {
+        this.nPorts = nPorts;
+        this.cacheSize = cacheSize;
+        this.routeCache = new RouteCache(cacheSize);
+    }
+
+    /**
+     * Add a routing rule to the router. Each rule associates an IP Address prefix with an output port.
+     * In case rules overlap, longest prefix wins.  If two rules specify exactly the same prefix, then
+     * the second rule triggers an IllegalArgumentException.  The port must be in the permitted range
+     * for this router, or an IllegalArgumentException will be triggered as well.
+     * 
+     * @param  prefix    an IP Address prefix in CIDR (dotted decimal) notation
+     * @param  port
+     * @return        true if rule is accepted. 
+     */
+    public void addRule(String prefix, int port) {
+        // "finish this (1 or 2 lines)" Kelly
+        // convert prefix to bits and then insert to trie:
+        // so in the trie i want to be comparing the prefix which is the key, but returning the value which is the port. don't really care what the content of the port is then.
+        IPAddress cidrToBits = new IPAddress(prefix);
+        if (map.get(port) != null) {
+            IPAddress oldAddress = map.get(port);
+            if (cidrToBits.size() > oldAddress.size() ) {
+                deleteRulePrivate(oldAddress);
+            }
+            if (cidrToBits.equals(oldAddress) ) {
+                throw new IllegalArgumentException();
+            }
+        }
+        trie.put(cidrToBits, port);
+        bigMap.put(cidrToBits , port);
+        map.put(port , cidrToBits);
+        // routeCache.updateCache(cidrToBits, port);
+    }
+
+    private void deleteRulePrivate(IPAddress address) {
+        // finish this (1 or 2 lines)
+        trie.delete(address);
+        Integer port = bigMap.get(address);
+        map.remove(port);
+    }
+
+    public void deleteRule(String prefix) {
+        // finish this (1 or 2 lines)
+        IPAddress cidrToBits = new IPAddress(prefix);
+        trie.delete(cidrToBits);
+        Integer port = bigMap.get(cidrToBits);
+        map.remove(port);
+    }
+
+    /**
+     * Simulate routing a packet to its output port based on a binary IP Address.
+     * If no rules apply to an address, route it to port -1 and log an error to System.err
+     * 
+     * @param  address    an IP Address object
+     * @return  number of output port 
+     */
+    public int getRoute(IPAddress address) {
+        // finish this (~6 lines)
+        if (trie.get(address) == null) {
+            routeCache.updateCache(address, -1);
+            System.err.println("no rules apply.");
+            return -1;
+        }
+        if (cacheCounter < cacheSize) {
+            cacheCounter ++;
+        }
+        int port = trie.get(address);
+        routeCache.updateCache(address, port);
+        return port;
+    }
+
+    /**
+     * Tell whether an IP Address is currently in the cache of most recently routed addresses
+      * 
+     * @param  address    an IP Address in dotted decimal notation
+     * @return  number of output port 
+     */
+    boolean isCached(IPAddress address) {
+        // finish this 
+        if (routeCache.lookupRoute(address) == null) {
+            return false;
+        }
+        return true;
+        // return false;
+    }
+    
+    /**
+     * For testing and debugging, return the contents of the LRU queue in most-recent-first order,
+     * as an array of IP Addresses.  Return a zero length array if the cache is empty
+     * 
+     */
+    IPAddress[] dumpCache() {
+        // put your code here
+        if (this.cacheSize == 0 || cacheCounter == 0) {
+            IPAddress [] emptyArray = new IPAddress [0];
+            return emptyArray;
+        }
+        // your code goes here
+        IPAddress [] array = new IPAddress [this.cacheSize];
+        String [] arrayAsString = routeCache.dumpQueue();
+        for (int i = 0; i < arrayAsString.length; i ++) {
+            String cidr = arrayAsString[i];
+            IPAddress address = new IPAddress(cidr);
+            array[i] = address;
+        }
+        return array;
+    }
+    
+    /**
+     * For testing and debugging, load a routing table from a text file
+     * 
+     */
+    public void loadRoutes(String filename) throws FileNotFoundException
+    {
+        Scanner sc = new Scanner(new File(filename));
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            if (line.startsWith("+")) {
+                String[] pieces = line.substring(1).split(",");
+                int port = Integer.parseInt(pieces[1]);
+                this.addRule(pieces[0].trim(), port);
+            }
+        }
+    }
+}
---
+++ DataStructures/Final/src/main/java/Final/code/RouteCache.java
@@ -0,0 +1,152 @@
+package Final.code;
+
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * This is a bounded cache that maintains only the most recently accessed IP Addresses
+ * and their routes.  Only the least recently accessed route will be purged from the
+ * cache when the cache exceeds capacity.  There are 2 closely coupled data structures:
+ *   -  a Map keyed to IP Address, used for quick lookup
+ *   -  a Queue of the N most recently accessed IP Addresses
+ * All operations must be O(1).  A big hint how to make that happen is contained
+ * in the type signature of the Map on line 38.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RouteCache
+{
+    // instance variables - add others if you need them
+    // do not change the names of any fields as the test code depends on them
+    
+    // Cache total capacity and current fill count.
+    private final int capacity;
+    private int nodeCount;
+    
+    // private class for nodes in a doubly-linked queue
+    // used to keep most-recently-used data
+    private class Node {
+        private Node prev, next;
+        private final IPAddress elem; 
+        private final int route;
+
+        Node(IPAddress elem, int route) {
+            prev = next = null;
+            this.elem = elem;
+            this.route = route;
+        }  
+    }
+    private Node head = null;
+    private Node tail = null;
+    private Map<IPAddress, Node> nodeMap; // the cache itself
+
+    /**
+     * Constructor for objects of class RouteCache
+     */
+    public RouteCache(int cacheCapacity) {
+        // your code goes here
+        nodeMap = new HashMap <IPAddress, Node> ();
+        this.capacity = cacheCapacity;
+    }
+
+    /**
+     * Lookup the output port for an IP Address in the cache.
+     * 
+     * @param  addr   a possibly cached IP Address
+     * @return     the cached route for this address, or null if not found 
+     */
+    public Integer lookupRoute(IPAddress addr) {
+        // your code goes here
+        if (nodeMap.get(addr) == null) {
+            return null;
+        }
+        Node node = nodeMap.get(addr);
+        Integer route = node.route;
+        return route;
+        // return null;
+     }
+     
+    /**
+     * Update the cache each time an element's route is looked up.
+     * Make sure the element and its route is in the Map.
+     * Enqueue the element at the tail of the queue if it is not already in the queue.  
+     * Otherwise, move it from its current position to the tail of the queue.  If the queue
+     * was already at capacity, remove and return the element at the head of the queue.
+     * 
+     * @param  elem  an element to be added to the queue, which may already be in the queue. 
+     *               If it is, don't add it redundantly, but move it to the back of the queue
+     * @return       the expired least recently used element, if any, or null
+     */
+    public IPAddress updateCache(IPAddress elem, int route) {
+        // your code goes here
+        IPAddress deletedAddy = null;
+        if (nodeMap.get(elem) != null) {
+            // I am just adjusting what is currently going on in the queue:
+            Node newNode = nodeMap.get(elem);
+            if (tail.equals(newNode) ) { return null; }
+            if (head.equals(newNode) ) {
+                head = newNode.next;
+                head.prev = newNode;
+                newNode.next = tail;
+                tail = newNode;    
+                return null;            
+            }
+            Node currentNode = newNode.prev;
+            newNode.prev.next = newNode.next;
+            newNode.prev = tail;
+            newNode.next = head;
+            tail.next = newNode;
+            tail = newNode;
+            return null;
+        }
+        if (nodeCount == capacity ) {
+            // I'm gonna go over capacity now:
+            deletedAddy = head.elem;
+            head = head.next;
+            nodeCount --;
+            nodeMap.remove(deletedAddy);
+        }
+        // // happy path of adding an obj to the queue:
+        Node newNode = new Node (elem, route);
+        nodeCount ++;
+        if (nodeCount == 1) { 
+            tail = newNode; 
+            head = newNode;
+        }
+        else {
+            newNode.prev = tail;
+            newNode.next = head;
+            tail.next = newNode;
+            tail = newNode;
+        }
+        nodeMap.put(elem , newNode);
+    	return deletedAddy; //placeholder
+    }
+
+    
+    /**
+     * For testing and debugging, return the contents of the LRU queue in most-recent-first order,
+     * as an array of IP Addresses in CIDR format. Return a zero length array if the cache is empty
+     * 
+     */
+    String[] dumpQueue() {
+        if (this.capacity == 0) {
+            String [] emptyArray = new String [0];
+            return emptyArray;
+        }
+        // your code goes here
+        String [] array = new String [this.capacity];
+        Node currentNode = tail;
+        int i = 0;
+        while (currentNode != null && i < this.capacity) {
+            array[i] = currentNode.elem.toCIDR();
+            i ++;
+            if (currentNode.prev == null || currentNode.prev.equals(tail) ) {
+                break;
+            }
+            currentNode = currentNode.prev;
+        }
+    	return array;
+    }
+}
