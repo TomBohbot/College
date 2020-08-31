20a0784b673e8d8c573f194df2bd3ea6b07f6810
BohbotTom <tbohbot@mail.yu.edu>
2020-08-31 16:21:21 -0700

    Adjusted code so that lists can be sorted either way

--- IntroToAlgorithms/assignments/SearchWithATweak/src/main/java/edu/yu/introtoalgs/SearchWithATweak.java
+++ IntroToAlgorithms/assignments/SearchWithATweak/src/main/java/edu/yu/introtoalgs/SearchWithATweak.java
@@ -26,17 +26,51 @@ public class SearchWithATweak {
     */
     public static int findFirstInstance (final List<Integer> list , final int key) {
         // The following code is O(logn):
-        if (list == null) {
+        if (list == null || list.size() == 0) {
             return -1;
         }
-        ArrayList <Integer> newList = (ArrayList <Integer>) list;
+        int lastElem = list.size() - 1;
+        if (list.get(0) < list.get(lastElem) ) {
+            return ascendingList(list, key);
+        }
+        else if (list.get(0) > list.get(lastElem) ) {
+            return descendingList(list, key);
+        }
+        else if (list.get(0) == key) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    private static int ascendingList (final List<Integer> list , final int key) {
         int low = 0;
-        int high = newList.size() - 1;
+        int high = list.size() - 1;
         int returnValue = -1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (key < newList.get(mid) ){ high = mid - 1; }
-            else if (key > newList.get(mid) ){ low = mid + 1; }
+            if (key < list.get(mid) ){ high = mid - 1; }
+            else if (key > list.get(mid) ){ low = mid + 1; }
+            else { 
+                returnValue = mid;
+                high = mid - 1;
+            }
+        }
+        if (returnValue != -1) {
+            return returnValue;
+        }
+        return -1;
+    }
+
+    private static int descendingList (final List<Integer> list , final int key) {
+        int low = 0;
+        int high = list.size() - 1;
+        int returnValue = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (key < list.get(mid) ){ low = mid + 1; }
+            else if (key > list.get(mid) ){ high = mid - 1; }
             else { 
                 returnValue = mid;
                 high = mid - 1;
@@ -61,84 +95,49 @@ public class SearchWithATweak {
     */
    public static int elementEqualToItsIndex (final List <Integer> list) {
         // The following code is O(logn):
-        if (list == null) {
+        if (list == null || list.size() == 0) {
             return -1;
         }
-        ArrayList <Integer> newList = (ArrayList <Integer>) list;
+        int lastElem = list.size() - 1;
+        if (list.get(0) < list.get(lastElem) ) {
+            return equalToIndexAsceding(list);
+        }
+        else if (list.get(0) > list.get(lastElem) ) {
+            return equalToIndexDescending(list);
+        }
+        else if (list.get(0) <= lastElem) {
+            return list.get(0);
+        }
+        else {
+            return -1;
+        }
+   }
+
+   private static int equalToIndexAsceding (final List <Integer> list) {
         int low = 0;
-        int high = newList.size() - 1;
+        int high = list.size() - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (mid < newList.get(mid) ){ high = mid - 1; }
-            else if (mid > newList.get(mid) ){ low = mid + 1; }
+            if (mid < list.get(mid) ){ high = mid - 1; }
+            else if (mid > list.get(mid) ){ low = mid + 1; }
             else { 
-                // Must Verify that this is the earliest match: 
-                // Muting out following code per Piazza Question 10:
-                // while (mid != 0 && newList.get(mid - 1) == mid - 1) {
-                //     if (mid == 0) {
-                //         return mid;
-                //     }
-                //     mid -= 1;
-                // }
                 return mid; 
             }
         }
-        return -1;
+        return -1; 
    }
 
-//    private static int oldFindFirstInstance (final List<Integer> list , final int key) {
-//     // The following code is O(logn):
-//     ArrayList <Integer> newList = (ArrayList <Integer>) list;
-//     int low = 0;
-//     int high = newList.size() - 1;
-//     while (low <= high) {
-//         int mid = low + (high - low) / 2;
-//         if (key < newList.get(mid) ){ high = mid - 1; }
-//         else if (key > newList.get(mid) ){ low = mid + 1; }
-//         else { 
-//             // In the case of duplicates: Does this change the run time?
-//             while (mid != 0 && newList.get(mid - 1) == key) {
-//                 mid -= 1;
-//             }
-//             return mid;
-//         }
-//     }
-//     return -1;
-// }
-
-//     private static int findFirstInstanceBruteForce (final List<Integer> list , final int key) {
-//         // The following code is O(n):
-//         ArrayList <Integer> newList = (ArrayList <Integer>) list;
-//         for (int i = 0; i < newList.size(); i ++) {
-//             if (newList.get(i) == key) {
-//                 return newList.indexOf(key);
-//             }
-//         }
-//         return -1;
-//     }
-
-//    private static int elementEqualToItsIndexOn2 (final List <Integer> list) {
-//         // Following code O(n^2):
-//         ArrayList <Integer> newList = (ArrayList<Integer>) list;
-//         for (int i = 0; i < newList.size(); i ++) {
-//             for (int j = 0; j < newList.size(); j ++ ){
-//                 if (newList.get(i) == i ) {
-//                     return i;
-//                 }
-//             }       
-//         }
-//         return -1;
-//    }
-
-//    private static int elementEqualToItsIndexOn (final List <Integer> list) {
-//     // Following code O(n):
-//     ArrayList <Integer> newList = (ArrayList<Integer>) list;
-//     for (int i = 0; i < newList.size(); i ++) {
-//         if (newList.get(i) == i ) {
-//             return i;
-//         }
-//     }
-//     return -1;
-//    }
-
+   private static int equalToIndexDescending (final List <Integer> list) {
+        int low = 0;
+        int high = list.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid < list.get(mid) ){ low = mid + 1; }
+            else if (mid > list.get(mid) ){ high = mid - 1; }
+            else { 
+                return mid; 
+            }
+        }
+        return -1; 
+    }
 }
--- IntroToAlgorithms/assignments/SearchWithATweak/src/test/java/edu/yu/introtoalgs/SearchWithATweakTest.java
+++ IntroToAlgorithms/assignments/SearchWithATweak/src/test/java/edu/yu/introtoalgs/SearchWithATweakTest.java
@@ -268,4 +268,247 @@ public class SearchWithATweakTest {
        list.add(2);
        assertEquals(SearchWithATweak.findFirstInstance(list , 2) , 0);
        }
+
+    //    @Test
+    //    public void verifyEarliestMatch2() {
+    //     ArrayList <Integer> list = new ArrayList <>();
+    //     list.add(2);
+    //     list.add(2);
+    //     list.add(null);
+    //     list.add(2);
+    //     list.add(2);
+    //     assertEquals(SearchWithATweak.findFirstInstance(list , 2) , 0);
+    //     }   @Test
+
+    @Test
+   public void inverseListTest() {
+    ArrayList <Integer> list = new ArrayList <>();
+    list.add(6);
+    list.add(5);
+    list.add(4);
+    list.add(3);
+    list.add(2);
+    list.add(1);
+    assertEquals(SearchWithATweak.findFirstInstance(list , 2) , 4);
+    }
+
+    @Test
+    public void inverseListTestDups() {
+     ArrayList <Integer> list = new ArrayList <>();
+     list.add(6);
+     list.add(5);
+     list.add(4);
+     list.add(2);
+     list.add(2);
+     list.add(1);
+     assertEquals(SearchWithATweak.findFirstInstance(list , 2) , 3);
+     }
+//------------------------------------------------------------------------------------------------------------
+
+    @Test
+    public void regList() {
+        ArrayList <Integer> list = new ArrayList <>();
+        list.add(-2);
+        list.add(0);
+        list.add(2);
+        list.add(4);
+        list.add(6);
+        list.add(8);
+        assertEquals(SearchWithATweak.findFirstInstance(list , -10) , -1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , -2) , 0);
+        assertEquals(SearchWithATweak.findFirstInstance(list , -1) , -1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , 0) , 1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , 1) , -1);    
+        assertEquals(SearchWithATweak.findFirstInstance(list , 2) , 2);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , 6) , 4);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , 7) , -1);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , 8) , 5);      
+    }
+
+    @Test
+    public void regListDups() {
+        ArrayList <Integer> list = new ArrayList <>();
+        list.add(-2);
+        list.add(0);
+        list.add(2);
+        list.add(2);
+        list.add(5);
+        list.add(6);
+        list.add(8);
+        list.add(8);
+        assertEquals(SearchWithATweak.findFirstInstance(list , -10) , -1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , -2) , 0);
+        assertEquals(SearchWithATweak.findFirstInstance(list , -1) , -1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , 0) , 1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , 1) , -1);    
+        assertEquals(SearchWithATweak.findFirstInstance(list , 2) , 2);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , 6) , 5);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , 5) , 4);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , 8) , 6);      
+    }
+
+    @Test
+    public void regListInverse() {
+        ArrayList <Integer> list = new ArrayList <>();
+        list.add(25);
+        list.add(20);
+        list.add(15);
+        list.add(10);
+        list.add(5);
+        list.add(0);
+        list.add(-1);
+        list.add(-10);
+        assertEquals(SearchWithATweak.findFirstInstance(list , 100) , -1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , 25) , 0);
+        assertEquals(SearchWithATweak.findFirstInstance(list , 22) , -1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , 20) , 1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , 16) , -1);    
+        assertEquals(SearchWithATweak.findFirstInstance(list , 15) , 2);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , 0) , 5);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , 5) , 4);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , -1) , 6);      
+    }
+
+    @Test
+    public void regListDupsInverse() {
+        ArrayList <Integer> list = new ArrayList <>();
+        list.add(25);
+        list.add(25);
+        list.add(15);
+        list.add(15);
+        list.add(5);
+        list.add(0);
+        list.add(-10);
+        list.add(-10);
+        assertEquals(SearchWithATweak.findFirstInstance(list , 100) , -1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , 25) , 0);
+        assertEquals(SearchWithATweak.findFirstInstance(list , 22) , -1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , 20) , -1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , 16) , -1);    
+        assertEquals(SearchWithATweak.findFirstInstance(list , 15) , 2);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , 0) , 5);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , 5) , 4);      
+        assertEquals(SearchWithATweak.findFirstInstance(list , -10) , 6);      
+    }
+
+    @Test
+    public void onlyRepeats() {
+        ArrayList <Integer> list = new ArrayList <>();
+        list.add(25);
+        list.add(25);
+        list.add(25);
+        list.add(25);
+        list.add(25);
+        list.add(25);
+        assertEquals(SearchWithATweak.findFirstInstance(list , 100) , -1);     
+        assertEquals(SearchWithATweak.findFirstInstance(list , 25) , 0);
+        assertEquals(SearchWithATweak.findFirstInstance(list , 22) , -1);       
+    }
+
+    @Test
+    public void emptyListFirstMethod() {
+        ArrayList <Integer> list = new ArrayList <>();     
+        assertEquals(SearchWithATweak.findFirstInstance(list , -10) , -1);      
+    }
+
+    @Test
+    public void nullFirstMethod() {
+        assertEquals(SearchWithATweak.findFirstInstance(null , -10) , -1);      
+    }
+
+
+//------------------------------------------------------------------------------------------------------------
+
+@Test
+public void regListMethod2() {
+    ArrayList <Integer> list = new ArrayList <>();
+    list.add(-2);
+    list.add(0);
+    list.add(2);
+    list.add(4);
+    list.add(6);
+    list.add(8);
+    assertEquals(SearchWithATweak.elementEqualToItsIndex(list) , 2);     
+}
+
+@Test
+public void regListDupsMethodTwo() {
+    ArrayList <Integer> list = new ArrayList <>();
+    list.add(-2);
+    list.add(0);
+    list.add(2);
+    list.add(2);
+    list.add(5);
+    list.add(5);
+    list.add(8);
+    list.add(8);
+    assertTrue(SearchWithATweak.elementEqualToItsIndex(list) == 2 || SearchWithATweak.elementEqualToItsIndex(list) == 5);     
+}
+
+
+
+@Test
+public void regListInverseMethodTwo() {
+    ArrayList <Integer> list = new ArrayList <>();
+    list.add(25);
+    list.add(20);
+    list.add(15);
+    list.add(10);
+    list.add(8);
+    list.add(5);
+    list.add(-1);
+    list.add(-10);
+    assertTrue(SearchWithATweak.elementEqualToItsIndex(list) == 5);
+}
+
+// @Test
+// public void regListDupsInverseMethodTwo() {
+//     ArrayList <Integer> list = new ArrayList <>();
+//     list.add(25);
+//     list.add(25);
+//     list.add(15);
+//     list.add(15);
+//     list.add(5);
+//     list.add(5);
+//     list.add(-10);
+//     list.add(-10);
+//     assertTrue(SearchWithATweak.elementEqualToItsIndex(list) == 5);      
+// }
+
+@Test
+public void onlyRepeatsBadPaths() {
+    ArrayList <Integer> list = new ArrayList <>();
+    list.add(25);
+    list.add(25);
+    list.add(25);
+    list.add(25);
+    list.add(25);
+    list.add(25);
+    assertEquals(SearchWithATweak.elementEqualToItsIndex(list) , -1);     
+}
+
+@Test
+public void onlyRepeatshappyPaths() {
+    ArrayList <Integer> list = new ArrayList <>();
+    list.add(3);
+    list.add(3);
+    list.add(3);
+    list.add(3);
+    list.add(3);
+    list.add(3);
+    assertEquals(SearchWithATweak.elementEqualToItsIndex(list) , 3);     
+}
+
+@Test
+public void emptyListSecondMethod() {
+    ArrayList <Integer> list = new ArrayList <>();     
+    assertEquals(SearchWithATweak.findFirstInstance(list , -10) , -1);      
+}
+
+@Test
+public void nullSecondMethod() {
+    assertEquals(SearchWithATweak.findFirstInstance(null , -10) , -1);      
+}
+
+
 }
