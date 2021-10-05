99d3da8608b2c12a890b0768374773543fd87eda
BohbotTom <tbohbot@mail.yu.edu>
2021-10-05 17:44:25 -0400

    Updating HW

--- ArtificialIntelligence/search_files/part_2/search.py
+++ ArtificialIntelligence/search_files/part_2/search.py
@@ -17,7 +17,7 @@ def search(n):
             cost += 1
     return [0, 0, cost]
 
-def evaluate_search(n):
+def evaluate_search(board_size, n_simulations):
     """
     Evaluate the search function by running it 100 times and returning key statistics.
 
@@ -26,15 +26,15 @@ def evaluate_search(n):
     """
     total_cost = 0
     # Simulate the search algorithm 100 times and get the sum of max depth and num states:
-    for i in range(100):
-        total_cost += search(n)[2]
-    return total_cost/100
+    for i in range(n_simulations):
+        total_cost += search(board_size)[2]
+    return total_cost/n_simulations
 
 # Run simulations on a 2x2 and 3x3 and 4x4 board:
-for i in range(2, 4):
-    evaluation = evaluate_search(i)
-    print(f"Average cost for {i}x{i} sized board: {evaluation}")
-    print()
+# for i in range(2, 4):
+evaluation = evaluate_search(board_size=4, n_simulations=50)
+print(f"Average cost for {4}x{4} sized board: {evaluation}")
+    # print()
 
 
 
