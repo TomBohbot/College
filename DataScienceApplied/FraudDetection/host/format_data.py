a92952fc167ee6ceacc626dd59683d8b5f62ee9d
BohbotTom <tbohbot@mail.yu.edu>
2021-11-07 18:30:36 -0500

    User Friendly

--- DataScienceApplied/FraudDetection/host/format_data.py
+++ DataScienceApplied/FraudDetection/host/format_data.py
@@ -6,7 +6,6 @@ def get_mapped_vals(df, feature):
     map = {}
     for i in range(len(unique_vals) ):
         map[unique_vals[i] ] = i
-    print(map)
     return map
 
 def get_columns():
@@ -45,44 +44,15 @@ def get_columns():
     return column_names
 
 def transformations(df):
-     # Transormations to train_id:
-    boolean_found = {'NotFound': 0, 'Found': 1, None: 0}
-    boolean_new = {'New': 0, 'Found': 1, None: 0}
-    true_false = {'T': 2, 'F': 1, None: 0}
     device_type = {None: 0, 'mobile': 1, 'desktop': 2}
-    df['id_12'].replace(get_mapped_vals(df=df, feature='id_12'), inplace=True)
-    df['id_16'].replace(boolean_found, inplace=True)
-    df['id_27'].replace(boolean_found, inplace=True)
-    df['id_28'].replace(boolean_new, inplace=True)
-    df['id_29'].replace(boolean_found, inplace=True)
-    df['id_35'].replace(true_false, inplace=True)
-    df['id_36'].replace(true_false, inplace=True)
-    df['id_37'].replace(true_false, inplace=True)
-    df['id_38'].replace(true_false, inplace=True)
-    df['DeviceType'].replace(device_type, inplace=True)
-    df['id_30'].replace(get_mapped_vals(df=df, feature='id_30'), inplace=True)
-    df['id_31'].replace(get_mapped_vals(df=df, feature='id_31'), inplace=True)
-    df['id_33'].replace(get_mapped_vals(df=df, feature='id_33'), inplace=True)
-    df['DeviceInfo'].replace(get_mapped_vals(df=df, feature='DeviceInfo'), inplace=True)
-    df['id_15'].replace(get_mapped_vals(df=df, feature='id_15'), inplace=True)
-    df['id_23'].replace(get_mapped_vals(df=df, feature='id_23'), inplace=True)
-    df['id_34'].replace(get_mapped_vals(df=df, feature='id_34'), inplace=True)
-    # Transormations to train_txn:
-    true_false = {'T': 2, 'F': 1, None: 0}
-    df['M1'].replace(true_false, inplace=True)
-    df['M2'].replace(true_false, inplace=True)
-    df['M3'].replace(true_false, inplace=True)
-    df['M6'].replace(true_false, inplace=True)
-    df['R_emaildomain'].replace(get_mapped_vals(df=df, feature='R_emaildomain'), inplace=True)
-    df['M5'].replace(get_mapped_vals(df=df, feature='M5'), inplace=True)
-    df['M7'].replace(get_mapped_vals(df=df, feature='M7'), inplace=True)
-    df['M8'].replace(get_mapped_vals(df=df, feature='M8'), inplace=True)
-    df['M9'].replace(get_mapped_vals(df=df, feature='M9'), inplace=True)
-    df['P_emaildomain'].replace(get_mapped_vals(df=df, feature='P_emaildomain'), inplace=True)
-    df['ProductCD'].replace(get_mapped_vals(df=df, feature='ProductCD'), inplace=True)
-    df['card4'].replace(get_mapped_vals(df=df, feature='card4'), inplace=True)
-    df['card6'].replace(get_mapped_vals(df=df, feature='card6'), inplace=True)
-    df['M4'].replace(get_mapped_vals(df=df, feature='M4'), inplace=True)
+    prod_cd = {None: 0, 'C': 1, 'W': 2, 'H': 3, 'S': 4, 'R': 5}
+    card_4 = {'discover': 0, 'visa': 1, 'mastercard': 2, 'american express': 3}
+    card_6 = {'charge': 0, 'credit': 1, 'debit': 2}
+
+    df['DeviceType'].replace(device_type, inplace=True) # 432
+    df['ProductCD'].replace(card_4, inplace=True)       # 3
+    df['card4'].replace(card_4, inplace=True)           # 7
+    df['card6'].replace(card_6, inplace=True)           # 9
 
 def get_df(data):
     column_names = get_columns()
--- DataScienceApplied/FraudDetection/host/server.py
+++ DataScienceApplied/FraudDetection/host/server.py
@@ -11,11 +11,31 @@ model = pickle.load(open('xgb_model.txt','rb'))
 
 @app.route('/api',methods=['POST'])
 def predict():
+    default = [
+        2987016,86620,30,"H",1790,555,150,"visa",226,"debit",170,87,0,0,"aol.com","aol.com",1,1,0,0,0,1,0,1,0,1,1,0,1,1,0,0,0,0,0,0,0,26,0,0,0,0,0,0,0,"a","a","a","a","a","a","a","a","a",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,18,140,0,0,0,0,1805,49,64,0,0,0,0,0,0,15622.99023,169755.7969,0,0,0,515,5155,2840,0,0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7460,0,0,1,0,0,0,0,0,100,"NotFound","NotFound",-300,"Found","Found","Found",15,529,575,0,0,0,"na",0,0,0,"Found","Found","Mac OS X 10_11_6","chrome 62.0",24,"1280x800","match_status:2","T","F","T","T","desktop","MacOS","na"
+    ]
+
+    default = []
+    for i in range(433):
+        default.append(25)
+
     # Get the data from the POST request.
     data = request.get_json(force=True)
+    data = data['data']
+    print(data)
+
+    device_info = {None: 0, 'mobile': 1, 'desktop': 2}
+    prod_cd = {None: 0, 'C': 1, 'W': 2, 'H': 3, 'S': 4, 'R': 5}
+    card_4 = {'discover': 0, 'visa': 1, 'mastercard': 2, 'american express': 3}
+    card_6 = {'charge': 0, 'credit': 1, 'debit': 2}
+    default[432] = device_info.get(data[0].lower() )
+    default[3] = device_info.get(data[1].lower() )
+    default[7] = device_info.get(data[2].lower() )
+    default[9] = device_info.get(data[3].lower() )
+    default[2] = device_info.get(data[4] )
 
     # Transform the data to match model's training data:
-    df = format_data.get_df(data['data'])
+    df = format_data.get_df(default)
 
     # Make prediction using xgb model
     prediction = model.predict_proba(df)
