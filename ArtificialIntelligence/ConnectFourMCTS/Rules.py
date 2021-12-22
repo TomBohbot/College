25cae81159c21335537511aeab93f350f6a22964
BohbotTom <tbohbot@mail.yu.edu>
2021-12-21 20:20:00 -0500

    V3

---
+++ ArtificialIntelligence/ConnectFourMCTS/Rules.py
@@ -0,0 +1,128 @@
+import random
+import PotentialLayouts as pot_layout
+import Gui as gui
+
+def choose_turn():
+    """
+    This function decides the original turn order 
+    between player 1 and player 2.
+
+    Input: None
+    Output: Random number deciding if player one is first or second.
+    """
+    return random.randint(1,2)
+
+def get_opponent(player):
+    """
+    This function returns the opponent of the current player.
+
+    Input: Current Player
+    Output: Opposing Player
+    """
+    if player == 1:
+        return 2
+    else:
+        return 1
+
+def vertical_win(board, position, player):
+    """
+    This function checks if the user's token
+    has just made a connect 4 vertically.
+
+    Input: 2d array board, 
+           coordinaes of placed token, 
+           players turn
+    Output: Boolean deciding whether the player has won.
+    """
+    row, col = position
+    pot_rows = pot_layout.vertical_layout(row)
+    for i in range(len(pot_rows)-2):
+        layout = [board[pot_rows[i], col], board[pot_rows[i+1], col], board[pot_rows[i+2], col]]
+        if layout.count(player) >= 3:
+            gui.visualize_winning_tokens( board, [(pot_rows[i], col), (pot_rows[i+1], col), (pot_rows[i+2], col), position])
+            return True
+    return False        
+
+def horizantal_win(board, position, player):
+    """
+    This function checks if the user's token
+    has just made a connect 4 horizantally.
+
+    Input: 2d array board, 
+           coordinaes of placed token, 
+           players turn
+    Output: Boolean deciding whether the player has won.
+    """
+    row, col = position
+    pot_cols =  pot_layout.horizantal_layout(col)
+    for i in range(len(pot_cols)-2 ):
+        layout = [board[row, pot_cols[i]], board[row, pot_cols[i+1]], board[row, pot_cols[i+2]]]
+        if layout.count(player) >= 3:
+            gui.visualize_winning_tokens( board, [(row, pot_cols[i]), (row, pot_cols[i+1]), (row, pot_cols[i+2]), position])
+            return True
+    return False
+
+def right_diag_win(board, position, player):
+    """
+    This function checks if the user's token
+    has just made a connect 4 in a right diagonal.
+
+    Input: 2d array board, 
+           coordinaes of placed token, 
+           players turn
+    Output: Boolean deciding whether the player has won.
+    """   
+    row, col = position
+    pot_coords = pot_layout.r_diag_layout(row, col)
+    if pot_coords == None:
+        return 0
+    for i in range(len(pot_coords)-2):
+        layout = [board[pot_coords[i][0], pot_coords[i][1] ], board[pot_coords[i+1][0], pot_coords[i+1][1] ], board[pot_coords[i+2][0], pot_coords[i+2][1] ] ]
+        if layout.count(player) >= 3:      
+            gui.visualize_winning_tokens( board, [(pot_coords[i][0], pot_coords[i][1]), (pot_coords[i+1][0], pot_coords[i+1][1]), (pot_coords[i+2][0], pot_coords[i+2][1]), position])
+            return True
+    return False
+
+def left_diag_win(board, position, player):
+    """
+    This function checks if the user's token
+    has just made a connect 4 in a left diagonal.
+
+    Input: 2d array board, 
+           coordinaes of placed token, 
+           players turn
+    Output: Boolean deciding whether the player has won.
+    """
+    row, col = position
+    pot_coords = pot_layout.l_diag_layout(row, col)
+    if pot_coords == None:
+        return 0
+    for i in range(len(pot_coords)-2):
+        layout = [board[pot_coords[i][0], pot_coords[i][1] ], board[pot_coords[i+1][0], pot_coords[i+1][1] ], board[pot_coords[i+2][0], pot_coords[i+2][1] ] ]
+        if layout.count(player) >= 3:
+            gui.visualize_winning_tokens( board, [(pot_coords[i][0], pot_coords[i][1]), (pot_coords[i+1][0], pot_coords[i+1][1]), (pot_coords[i+2][0], pot_coords[i+2][1]), position])            
+            return True
+    return False
+
+def won_game(board, position, player):
+    """
+    This function checks if the user's token
+    has just made a connect 4.
+
+    Input: 2d array board, 
+           coordinaes of placed token, 
+           players turn
+    Output: Boolean deciding whether the player has won.
+    """
+    # if position[0] < 0 or position[1] < 0:
+    #     return False
+    if vertical_win(board, position, player):
+        return True
+    if horizantal_win(board, position, player):
+        return True
+    if right_diag_win(board, position, player):
+        return True
+    if left_diag_win(board, position, player):
+        return True
+    else:
+        return False
---
+++ ArtificialIntelligence/ConnectFourMCTS/monte_carlo_search_tree.py
@@ -0,0 +1,132 @@
+import Move_Piece as mp
+import Rules as rules
+import SimRules as sim_rules
+import math
+
+class Node:
+    def __init__(self, parent=None, children=[], move=-1):
+        self.parent = parent
+        self.children = children
+        self.move = move
+        self.n_samples = 0
+        self.n_games_won = 0
+
+total_samples = 0
+
+def ucb(node):
+    """
+    Calculate a confidence interval for win ratio of each node in tree.
+    """
+    if node.n_samples == 0:
+        return math.inf
+    return (node.n_games_won) + (2)*math.sqrt( (math.log(total_samples) / node.n_samples) )
+
+def simulation(board, player_one):
+    """ 
+    Iterate until you arrive at a terminal state.
+    If you are not at a terminal state then you 
+    take a random action and simulate until you 
+    arrive at a terminal state.
+
+    Output: Winner of the Connect 4 Game
+    """
+    game_in_progress = True
+    curr_player = player_one
+    winner = None
+    counter = 0
+    while game_in_progress and mp.full_board(board) == False:
+        counter += 1
+        position = mp.random_next_move(board, curr_player)
+        if sim_rules.won_game(board, position, curr_player):
+            game_in_progress = False
+            winner = curr_player
+            return True if winner == player_one else False
+        curr_player = sim_rules.get_opponent(curr_player)
+    return False
+
+def back_propagation(node, won_game):
+    # If the game was won then must uptick 
+    val = 0
+    if won_game == True:
+        val = 1
+    while True:
+        if node.parent == None:
+            node.n_samples += 1
+            node.n_games_won += val
+            total_samples += 1
+            break
+        node.n_samples += 1
+        node.n_games_won += val
+        node = node.parent
+    
+def expansion(node, board, player):
+    for i in range(7):
+        if mp.valid_location(board=board, col=i):
+            child_node = Node(parent=node, move=i)
+            node.children.append(child_node)
+
+def search(node, orig_board, player):
+    while True:
+        # Check if the root is a leaf node:
+        if len(node.children) == 0:
+            # If the node has been sampled zero times then jump to simulation:
+            if node.n_samples == 0:
+                # Clone board:
+                board = orig_board.copy()
+                # Make a random move on cloned board:
+                col = node.move
+                mp.next_move(board=board, player=player, col=col)
+                # Simulate Game
+                won_game = simulation(board=board, player_one=player)
+                # Call back propagation to uptick number of samples and won games:
+                back_propagation(node=node, won_game=won_game)
+            # If it has been visited then do expansion:
+            else:
+                expansion(node=node)
+                # Clone board:
+                board = orig_board.copy()
+                # Make a random move on cloned board:
+                mp.next_move(board=board, player=player, col=node.children[0].move)
+                # Simulate Game
+                won_game = simulation(board=board, player_one=player)
+                # Call back propagation to uptick number of samples and won games:
+                back_propagation(node=node, won_game=won_game)
+            break
+        # Find the largest ucb value between children nodes:
+        max_ucb_score = -1
+        node_to_explore = None
+        for child_node in node.children:
+            curr_ucb_score = ucb(child_node)   
+            if curr_ucb_score > max_ucb_score:
+                max_ucb_score = curr_ucb_score
+                node_to_explore = child_node
+        # Set the current node to the node with highest ucb score:
+        node = node_to_explore
+        break
+
+def mcst(board, player):
+    # Set a number of iterations to run the search:
+    n_iterations = 100000
+    # Create a root node
+    root_node = Node()
+    # Give the root node children whose move are available:
+    for i in range(7):
+        if mp.valid_location(board=board, col=i):
+            child_node = Node(parent=root_node, move=i, children=[])
+            root_node.children.append(child_node)
+    # Perform the search for n_iteration times
+    for i in range(n_iterations):
+        search(node=root_node, orig_board=board, player=player)
+    # Find the largest amount of wins in the next move:
+    max_score = -1
+    best_col = None
+    for child_node in root_node.children:
+        curr_score = child_node.n_games_won
+        if curr_score > max_score and mp.valid_location(board=board, col=child_node.move) == True:
+            max_score = curr_score
+            best_col = child_node.move
+    return best_col
+
+def next_move(board, player):
+    best_move = mcst(board, player)
+    return mp.next_move(board=board, player=player, col=best_move)
